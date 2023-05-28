3c81590f1e134b0e1d035f8b68606ca2742373bd
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-05-28 23:40:55 +0100

    Remove unused imports and reorganise imports

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,19 @@
-from flask import Flask
 from threading import Thread
+
+from flask import Flask
 
 app = Flask('')
 
 
 @app.route('/')
 def home():
-	return "I'm alive"
+    return "I'm alive"
 
 
 def run():
-	app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 
 def keep_alive():
-	t = Thread(target=run)
-	t.start()
+    t = Thread(target=run)
+    t.start()
