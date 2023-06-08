8bff05caf7546ab23c95722a1a29f7c210fa7225
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-06-08 13:37:08 +0100

    Fix update_stats on hourly basis by providing datetime to function

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ async def send_message_at_midnight():
 
         # if now.hour == 0 or now.hour == 6 or now.hour == 12 or now.hour == 18:
         weekly_reset = now.weekday() == 0 and now.hour == 0
-        update_stats(client, weekly_reset)
+        update_stats(client, now, weekly_reset)
 
 
 @client.event
