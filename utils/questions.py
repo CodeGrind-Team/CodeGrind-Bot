d8da8300e34899216f1a7b5fd29dacc84c89631f
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-06-18 13:22:31 +0100

    Handle requests exceptions such as ReadTimeout

--- utils/questions.py
+++ utils/questions.py
@@ -7,6 +7,9 @@ from bot_globals import logger
 
 
 def daily_question_embed() -> discord.Embed:
+    logger.info(
+        "file: utils/questions.py ~ daily_question_embed ~ run")
+
     url = 'https://leetcode.com/graphql'
 
     headers = {
@@ -30,7 +33,25 @@ def daily_question_embed() -> discord.Embed:
     '''
     }
 
-    response = requests.post(url, json=data, headers=headers, timeout=10)
+    try:
+        response = requests.post(url, json=data, headers=headers, timeout=5)
+    except Exception as e:
+        logger.exception(
+            "file: utils/questions.py ~ daily_question_embed ~ exception: %s", e)
+
+        embed = discord.Embed(title="Daily problem",
+                              color=discord.Color.blue())
+
+        embed.description = "Daily problem could not be retrieved"
+
+        return embed
+
+    if response.status_code != 200:
+        embed = discord.Embed(title="Daily problem",
+                              color=discord.Color.blue())
+
+        embed.description = "Daily problem could not be retrieved"
+
     response_data = response.json()
 
     # Extract and print the link
@@ -53,7 +74,7 @@ def daily_question_embed() -> discord.Embed:
 
 def get_problems_solved_and_rank(leetcode_username: str) -> dict[str, Any] | None:
     logger.info(
-        "file: cogs/stats.py ~ get_problems_solved_and_rank ~ run ~ leetcode_username: %s", leetcode_username)
+        "file: utils/questions.py ~ get_problems_solved_and_rank ~ run ~ leetcode_username: %s", leetcode_username)
 
     url = 'https://leetcode.com/graphql'
 
@@ -100,18 +121,28 @@ def get_problems_solved_and_rank(leetcode_username: str) -> dict[str, Any] | Non
     # }
 
     logger.info(
-        "file: cogs/stats.py ~ get_problems_solved_and_rank ~ data requesting ~ https://leetcode.com/%s", leetcode_username)
+        "file: utils/questions.py ~ get_problems_solved_and_rank ~ data requesting ~ https://leetcode.com/%s", leetcode_username)
 
-    response = requests.post(
-        url, json=data, headers=headers, timeout=10)
+    try:
+        response = requests.post(
+            url, json=data, headers=headers, timeout=5)
+    except Exception as e:
+        logger.exception(
+            "file: utils/questions.py ~ get_problems_solved_and_rank ~ exception: %s", e)
+        return None
+
+    if response.status_code != 200:
+        return None
+
     response_data = response.json()
+
     if response_data["data"]["matchedUser"] is None:
         logger.warning(
-            "file: cogs/stats.py ~ get_problems_solved_and_rank ~ user not found ~ https://leetcode.com/%s", leetcode_username)
+            "file: utils/questions.py ~ get_problems_solved_and_rank ~ user not found ~ https://leetcode.com/%s", leetcode_username)
         return None
 
     logger.info(
-        "file: cogs/stats.py ~ get_problems_solved_and_rank ~ data requested successfully ~ https://leetcode.com/%s", leetcode_username)
+        "file: utils/questions.py ~ get_problems_solved_and_rank ~ data requested successfully ~ https://leetcode.com/%s", leetcode_username)
 
     stats = response_data["data"]["matchedUser"]
 
