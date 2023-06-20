0ead83c91d60a4658647396303cff59323e99024
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-06-20 20:08:15 +0100

    Fix bot not workning on new servers by intialising json if it doesn't exist

--- utils/io_handling.py
+++ utils/io_handling.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 from typing import Any
 
 from utils.run_blocking import to_thread
@@ -11,7 +12,17 @@ def read_file(path: str) -> Any | None:
         with open(path, 'r', encoding="UTF-8") as file:
             data = json.load(file)
         return data
-    return None
+
+    data = {
+        "users": {},
+        "last_updated": datetime.now().strftime("%d/%m/%Y %H:%M"),
+        "channels": []
+    }
+
+    with open(path, "w", encoding="UTF-8") as file:
+        json.dump(data, file, indent=4)
+
+    return data
 
 
 @to_thread
