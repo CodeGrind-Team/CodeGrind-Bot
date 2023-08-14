6bf3ce2be8cf915d241fadd738cca4a3c665a5c8
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-08-14 14:33:40 +0100

    Add option to force update stats, daily reset, and weekly reset on start (#55)

--- main.py
+++ main.py
@@ -21,10 +21,20 @@ async def on_autopost_success():
 
 
 @client.event
+async def on_ready() -> None:
+    logger.info("file: main.py ~ on_ready ~ start")
+
+    update_stats_on_start = os.environ["UPDATE_STATS_ON_START"] == "True"
+    daily_reset_on_start = os.environ["DAILY_RESET_ON_START"] == "True"
+    weekly_reset_on_start = os.environ["WEEKLY_RESET_ON_START"] == "True"
+
+    if update_stats_on_start or daily_reset_on_start or weekly_reset_on_start:
+        await send_daily_question_and_update_stats(update_stats_on_start, daily_reset_on_start, weekly_reset_on_start)
+
+@client.event
 async def setup_hook() -> None:
-    logger.info("file: main.py ~ on_ready ~ %s",
-                datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S"))
-    logger.info("file: main.py ~ on_ready ~ logged in as a bot %s", client.user)
+    logger.info("file: main.py ~ setup_hook ~ start")
+    logger.info("file: main.py ~ setup_hook ~ logged in as a bot %s", client.user)
 
     if os.environ["PRODUCTION"] == "True":
         dbl_token = os.environ["TOPGG_TOKEN"]
@@ -34,17 +44,12 @@ async def setup_hook() -> None:
     try:
         synced = await client.tree.sync()
         logger.info(
-            "file: main.py ~ on_ready ~ synced %s commands", len(synced))
+            "file: main.py ~ setup_hook ~ synced %s commands", len(synced))
 
-        # TODO: re-enable force update
-        update_stats_on_start = os.environ["UPDATE_STATS_ON_START"] == "True"
-        daily_reset = os.environ["DAILY_RESET"] == "True"
-        weekly_reset = os.environ["WEEKLY_RESET"] == "True"
-
-        send_daily_question_and_update_stats.start()
+        send_daily_question_and_update_stats_schedule.start()
 
     except Exception as e:
-        logger.exception("file: main.py ~ on_ready ~ exception: %s", e)
+        logger.exception("file: main.py ~ setup_hook ~ exception: %s", e)
 
 
 async def load_extensions() -> None:
