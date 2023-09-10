7b579aa8ec394a1ff73139373b5d147b83772050
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-09-10 18:28:13 +0100

    Update command ids, and improve and fix codegrind team /help section.

--- embeds/general_embeds.py
+++ embeds/general_embeds.py
@@ -58,15 +58,12 @@ Credit to [LeetCode Stats Card](https://github.com/JacobLinCool/LeetCode-Stats-C
 >    - `difficulty` _(optional)_: A difficulty level ('easy', 'medium' or 'hard'). Default is a 'random' difficulty level.
 
 </question search:1150096353133875251>: Returns the question according to the name, ID, or URL given
->    - `name_id_or_url` : The LeetCode question name, ID, or URL.
-
-Credit to [Aalaap](<https://www.linkedin.com/in/aalaap-d-969703239/>) for contributing this feature.
-""",
+>    - `name_id_or_url` : The LeetCode question name, ID, or URL.""",
 
                       "Roles": """
 ## Roles
 
-If an admin has enabled this feature using </roles enable:1142272770131107952>, you will receive the following roles.
+If an admin has enabled this feature using </roles enable:1147624135850197033>, you will receive the following roles.
 
 `CodeGrind Verified`: Given to you if you've connected your account to the bot in this server using </add:1115756888185917542>.
 
@@ -85,9 +82,7 @@ If an admin has enabled this feature using </roles enable:1142272770131107952>, 
 >    - Streak Pursuer (7 Days)
 >    - Streak Adventurer (14 Days)
 >    - Streak Dominator (30 Days)
->    - Streak Legend (90 Days)
-
-Credit to [Shaan](<https://github.com/ShaanCoding>) for contributing this feature.""",
+>    - Streak Legend (90 Days)""",
 
                       "Admin": """
 # Admin commands
@@ -95,18 +90,18 @@ These commands are only available to administrators of this server.
 
 ## Roles
 
-</roles enable:1142272770131107952>: Create and give verified user, point milestones, and streak roles that will be automatically updated at midday (UTC).
+</roles enable:1147624135850197033>: Create and give verified user, point milestones, and streak roles that will be automatically updated at midday (UTC).
 >    - `CodeGrind Verified`: Given to user's that have connected their LeetCode account to the bot in this server.
 >    - `Point milestones`: Given to user's depending on their total CodeGrind score.
 >    - `Streak`: Given to user's depending on their streak. Their streak is incremented each day if the user has completed a new LeetCode question (not necessarily the LeetCode daily question).
 
-</roles disable:1142272770131107952> Delete and remove verified user, point milestones, and streak roles.
+</roles disable:1147624135850197033> Delete and remove verified user, point milestones, and streak roles.
 
 ## Notifications
 
-</notify-channel enable:1127663038514876541>: Set what notifications the channel receives.
+</notify-channel enable:1147630083704635392>: Set what notifications the channel receives.
 
-</notify-channel disable:1127663038514876541>: Stop channel from receiving selected notifications.
+</notify-channel disable:1147630083704635392>: Stop channel from receiving selected notifications.
 
 >    - `channel` _(optional)_: The channel (e.g. "#home-channel"). Default is the channel where the command was ran.
 
@@ -115,18 +110,20 @@ Note: please press the 'save' button twice
 ## Settings
 
 </settings timezone:1127663038514876538>: Change the timezone of the server.
->    - `timezone`: A timezone (case sensitive) from the following list of timeones: https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568 .
+>    - `timezone`: A timezone (case sensitive) from the following list of timeones: https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568 .""",
 
+                      "CodeGrind Team": """
 ## CodeGrind Team
 
-### Developers:
-Nano: <@814656377188384810> 
-Kevin: <@335154148976885770>
+### Developers
+Nano (<@814656377188384810>)
+Kevin (<@335154148976885770>)
 
-### Contributors:
-Aalaap: <@635232267458969631> - Worked on the </search-question:1147609257735364668> command
-Shaan: <@199662959964848128> - Worked on the </roles:1142272770131107952> command
-"""}
+### Contributors
+[Shaan](<https://github.com/ShaanCoding>) (<@199662959964848128>): Created the </roles enable:1147624135850197033> and </roles disable:1147624135850197033> commands.
+[Aalaap](<https://www.linkedin.com/in/aalaap-d-969703239/>) (<@635232267458969631>): Created the </question daily:1150096353133875251>, </question random:1150096353133875251>, and </question search:1150096353133875251> commands. 
+
+Thank you to all the contributors for helping improve CodeGrind Bot!"""}
 
 
 def help_embed(description: str) -> discord.Embed:
