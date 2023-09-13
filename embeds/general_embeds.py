841a97d182ab0508d723e6c0824b01339276a232
Leo <95043714+leo-lungu@users.noreply.github.com>
2023-09-13 13:50:33 -0400

    Add Privacy Policy to /help (#76)

--- embeds/general_embeds.py
+++ embeds/general_embeds.py
@@ -16,7 +16,11 @@ Here is how much each question difficulty is worth:
 >    - Hard questions are worth {DIFFICULTY_SCORE['hard']} points
 
 ## Commands
-Please select a category from the dropdown to be shown all the commands related to the selected category.""",
+Please select a category from the dropdown to be shown all the commands related to the selected category.
+
+## Privacy Policy
+You can view our Privacy Policy [here](https://github.com/CodeGrind-Team/CodeGrind-Bot/blob/main/PRIVACY_POLICY.md).
+""",
                       "Account": """
 ## Account
 
