fd0fbad61c9b7bb206b9d5961325a53e2ad70046
svn27 <164501448+svn27@users.noreply.github.com>
2024-06-17 18:37:57 +0100

    added language count and skill count to the Record document

--- database/models/record.py
+++ database/models/record.py
@@ -2,13 +2,19 @@ from datetime import datetime
 
 from beanie import Document, Granularity, TimeSeriesConfig
 
-from .user import Submissions
+from .user import Submissions, LanguageProblemCount, SkillProblemCount
+
+from typing import List, Optional
 
 
 class Record(Document):
     timestamp: datetime
     user_id: int
     submissions: Submissions
+    languages_problem_count: Optional[List[LanguageProblemCount]] = []
+    skills_problem_count_advanced: Optional[List[SkillProblemCount]] = []
+    skills_problem_count_intermediate: Optional[List[SkillProblemCount]] = []
+    skills_problem_count_fundamental: Optional[List[SkillProblemCount]] = []
 
     class Settings:
         name = "records"
