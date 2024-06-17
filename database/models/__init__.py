c31c6228fe5be5e9e51599a443cebd3da3f3296d
svn27 <164501448+svn27@users.noreply.github.com>
2024-06-17 18:39:25 +0100

    Updated __init__.py to export new models

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -1,4 +1,11 @@
 from .preference import Preference
 from .record import Record
 from .server import Channels, Server
-from .user import Stats, Submissions, User, Votes
+from .user import (
+    Stats,
+    Submissions,
+    User,
+    Votes,
+    SkillProblemCount,
+    LanguageProblemCount,
+)
